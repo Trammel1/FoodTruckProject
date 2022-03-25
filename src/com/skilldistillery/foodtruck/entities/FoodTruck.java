0a6ba0062a576732e89dfda1b203680f6ff44a7a
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-25 18:37:34 -0400

    initial commit

---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private static int nextTruckId;
+	private static int nextTruckName;
+	private static int nextFoodType;
+	private static int nextFoodRating;
+	
+}
